e1c10ad036dd34fc04ae6d4c4eae6505b2f00301
Vitaly Osipov <vital-nvl@mail.ru>
2022-12-13 13:04:11 +0300

    main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+#Запуск программы
+
 from console import console
 from rational import rational
 from complex import complex
+from logger import result_logger
 
 data = console()
 if data[3] == '1':
@@ -9,5 +12,6 @@ elif data[3] == '2':
     result = rational(data)
 else:
     result = 'Ошибка, неправильно введены данные!!!'
-
-print(f'Результат вычисления {data[0]} {data[2]} {data[1]} = {result}')
+print(f'Результат вычисления {data[0]} {data[2]} {data[1]} = {result}\n')
+print('Результат записан в файл log.txt')
+result_logger(data, result)
