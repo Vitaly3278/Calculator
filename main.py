c522a133bbbc086baa2865e300725b274769dd7a
Vitaly Osipov <vital-nvl@mail.ru>
2022-12-13 11:28:10 +0300

    main

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ from rational import rational
 from complex import complex
 
 data = console()
-result = complex(data)
-print(result)
+if data[3] == '1':
+    result = complex(data)
+elif data[3] == '2':
+    result = rational(data)
+else:
+    result = 'Ошибка, неправильно введены данные!!!'
+
+print(f'Результат вычисления {data[0]} {data[2]} {data[1]} = {result}')
